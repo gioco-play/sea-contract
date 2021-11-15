20c3fe05fba8a1f2a403f428e810a137c8ba5715
q90016200 <q90016200@gmail.com>
2021-11-15 11:55:04 +0800

    update sea

---
+++ src/Contract/SEAServiceInterface.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+namespace GiocoPlus\SEA\Contract;
+
+
+interface SEAServiceInterface
+{
+    /**
+     * 遊戲啟動
+     *
+     * @param string $op_code
+     * @param string $account
+     * @param string $game_code
+     * @param string $language
+     * @return mixed
+     */
+    function gameLaunch(string $op_code, string $account, string $game_code, string $language);
+
+    /**
+     * 遊戲詳情
+     * @param string $op_code
+     * @param string $account
+     * @param string $bet_id
+     * @return mixed
+     */
+    function gameDetail(string $op_code, string $account, string $bet_id);
+
+    /**
+     * 抓取遊戲紀錄
+     *
+     * @param string $op_code
+     * @param integer $past_minutes
+     * @param string $cache_key
+     * @return mixed
+     */
+    function betLogGrabber(string $op_code, int $past_minutes, string $cache_key);
+
+}
